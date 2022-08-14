554c47505802583ebc5ef71993dd68a66cb0f784
Captain Maramo <captain_maramo@pop-os.localdomain>
2022-08-14 12:48:36 +0200

    circular percent indicator is now filling on running app

--- lib/presentation/pomodoro/widgets/mainscreen/main_progress_circle_button.dart
+++ lib/presentation/pomodoro/widgets/mainscreen/main_progress_circle_button.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'dart:async';
 
-class ProgressCircleButton extends StatelessWidget {
+class ProgressCircleButton extends StatefulWidget {
   const ProgressCircleButton({
     Key? key,
   }) : super(key: key);
+  @override
+  State<ProgressCircleButton> createState() => _ProgressCircleButtonState();
+}
 
+class _ProgressCircleButtonState extends State<ProgressCircleButton> {
+  int secondsLeft = 10;
+  String secondsToMinAndSeconds() {
+    int mins = secondsLeft ~/ 60;
+    int seconds = secondsLeft % 60;
+    if (seconds < 10) {
+      return "$mins:0$seconds";
+    }
+    return "$mins:$seconds";
+  }
+
+  bool _isRunning = true;
   @override
   Widget build(BuildContext context) {
+    String timer = secondsToMinAndSeconds();
+
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        Timer.periodic(const Duration(seconds: 1), (Timer timer) {
+          if (!_isRunning) {
+            timer.cancel();
+          }
+          setState(() {
+            if (secondsLeft > 0) {
+              secondsLeft--;
+            } else {
+              timer.cancel();
+              secondsLeft = 1500;
+            }
+          });
+        });
+        super.initState();
+      },
       style: ElevatedButton.styleFrom(
         fixedSize: const Size(300, 300),
         shape: const CircleBorder(),
@@ -17,10 +50,10 @@ class ProgressCircleButton extends StatelessWidget {
       child: CircularPercentIndicator(
         radius: 150.0,
         lineWidth: 10.0,
-        percent: 1.0,
-        center: const Text(
-          "25:00",
-          style: TextStyle(fontSize: 64),
+        percent: 1 - secondsLeft / 1500,
+        center: Text(
+          timer,
+          style: const TextStyle(fontSize: 64),
         ),
         progressColor: const Color.fromRGBO(46, 125, 50, 1),
         backgroundColor: Colors.black,
--- lib/presentation/pomodoro/widgets/mainscreen/main_screen_body.dart
+++ lib/presentation/pomodoro/widgets/mainscreen/main_screen_body.dart
@@ -6,7 +6,7 @@ import 'main_four_pomodoro_prgogress_painter.dart';
 import 'main_progress_circle_button.dart';
 import 'main_todo_field.dart';
 
-class MainScreenBody extends StatelessWidget {
+class MainScreenBody extends StatefulWidget {
   const MainScreenBody({Key? key, required this.themeData, required this.box})
       : super(key: key);
 
@@ -14,13 +14,18 @@ class MainScreenBody extends StatelessWidget {
   final Box box;
 
   @override
+  State<MainScreenBody> createState() => _MainScreenBodyState();
+}
+
+class _MainScreenBodyState extends State<MainScreenBody> {
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: <Widget>[
           MainScreenToDoField(
-            themeData: themeData,
-            box: box,
+            themeData: widget.themeData,
+            box: widget.box,
           ),
           const Padding(
             padding: EdgeInsets.all(16.0),
